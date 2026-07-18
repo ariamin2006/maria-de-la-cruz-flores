e58b68fa525266e62a1978585ea5fbd62b8fbcd7
ariam <mrdelacruzflores@gmail.com>
2026-07-18 08:14:06 -0500

    Add files via upload

---
+++ SEMANA 4_SISTEMATIZACION DE LA CRUZ FLORES.R
@@ -0,0 +1,28 @@
+#SEMANA 4
+#DE LA CRUZ FLORES MARIA 
+#INSTALACION DE 5 PAQUETES 
+
+#INSTALANDO UN PAQUETE 
+
+install.packages("ggplot2")
+library(ggplot2)
+
+#INSTALANDO EL SEGUNDO PAQUETE 
+
+install.packages("dplyr")
+library(dplyr)
+
+#INSTALANDO EL TERCER PAQUETE 
+
+install.packages("janitor")
+library(janitor)
+
+#INSTALANDO EL CUARTO PAQUETE 
+
+install.packages("skimr")
+library(skimr)
+
+#INSTALANDO EL QUINTO PAQUETE 
+
+install.packages("rmarkdown")
+library(rmarkdown)
